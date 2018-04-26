2017b9e61a756b6baacb5a72a081ab54984da01d
Sabrout <omarsabrout@gmail.com>
2018-04-26 10:10:33 +0200

    Test dataset.py

--- scratch.py
+++ scratch.py
@@ -1,6 +1,34 @@
-import scipy.io as sio
+import numpy as np
+from dataset import Dataset
 
-content = sio.loadmat('data/books.dvd.X.mat')
 
-for i in content.get('X'):
-    print(i)
+def load_dat(path):
+    content = np.loadtxt(path, dtype=int)
+    if content.ndim == 1:
+        dataset = np.empty((len(content), 1), dtype=int)
+    else:
+        dataset = np.empty(content.shape, dtype=int)
+    for i in range(0, content.__len__()):
+        if content.ndim == 1:
+            dataset[i] = content[i]
+        else:
+            counter = 0
+            for j in content[i]:
+                if counter == 3:
+                    break
+                if j == ' ':
+                    continue
+                else:
+                    dataset[i][counter] = j
+                    counter += 1
+    return dataset
+
+
+X = load_dat('data/books/X.dat')
+Y = load_dat('data/books/Y.dat')
+
+dataset = Dataset(X, Y)
+
+print(dataset.get_nb_examples())
+print(dataset.get_nb_features())
+print(dataset.select_examples([2]))
