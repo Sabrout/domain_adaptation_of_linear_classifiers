45674a2fb34232b6a60117a760b660b727fd2416
Sabrout <omarsabrout@gmail.com>
2018-04-26 16:13:57 +0200

    Test learn function in dalc.py

--- scratch.py
+++ scratch.py
@@ -1,11 +1,12 @@
 import numpy as np
 from dataset import Dataset
+from dalc import Dalc
 
 
 def load_dat(path):
     content = np.loadtxt(path, dtype=int)
     if content.ndim == 1:
-        dataset = np.empty((len(content), 1), dtype=int)
+        dataset = np.empty((len(content), ), dtype=int)
     else:
         dataset = np.empty(content.shape, dtype=int)
     for i in range(0, content.__len__()):
@@ -24,11 +25,22 @@ def load_dat(path):
     return dataset
 
 
-X = load_dat('data/books/X.dat')
-Y = load_dat('data/books/Y.dat')
+X1 = load_dat('data/books/X.dat')
+Y1 = load_dat('data/books/Y.dat')
 
-dataset = Dataset(X, Y)
+source_dataset = Dataset(X1[:len(Y1)], Y1)
 
-print(dataset.get_nb_examples())
-print(dataset.get_nb_features())
-print(dataset.select_examples([2]))
+X2 = load_dat('data/dvd/X.dat')
+
+target_dataset = Dataset(X2[:len(Y1)])
+
+print((len(X1), len(Y1), len(X2)))
+print((len(source_dataset.X), len(source_dataset.Y), len(target_dataset.X)))
+
+# print(source_dataset.get_nb_examples())
+# print(source_dataset.get_nb_features())
+# print(source_dataset.select_examples([2]))
+
+dalc = Dalc()
+result = dalc.learn(source_dataset, target_dataset)
+print(result)
